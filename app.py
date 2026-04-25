4750cbd4f82b1173eac8d12730a0b6beca9d615f
Ankit <ankit00132@gmail.com>
2026-04-25 13:53:44 +0530

    Implemented NearestNeighbor Algo, @used st.cache, updated recommend function, added error-handling

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from src.content_based_filtering import recommend
 from scipy.sparse import load_npz
 import pandas as pd
+import joblib
 
 # transformed data path
 transformed_data_path = "data/processed/transformed_data.npz"
@@ -9,11 +10,38 @@ transformed_data_path = "data/processed/transformed_data.npz"
 # cleaned data path
 cleaned_data_path = "data/processed/cleaned_music_data.csv"
 
-# load the data
-data = pd.read_csv(cleaned_data_path)
+# trained model path
+model_path = 'models/nearest_neighbor.joblib'
 
-# load the transformed data
-transformed_data = load_npz(transformed_data_path)
+# using streamlit caching to save rerun
+@st.cache_data
+def load_dataframe(cleaned_data_path):
+    # load the data
+    data = pd.read_csv(cleaned_data_path)
+    song_to_index = {
+        name.lower() : idx for idx,name in enumerate(data["name"])
+    }
+    return data,song_to_index
+
+
+@st.cache_data
+def load_transformed_data(transformed_data_path):
+    # load the transformed data
+    transformed_data = load_npz(transformed_data_path)
+    return transformed_data
+
+
+
+@st.cache_resource
+def load_model(model_path):
+    model = joblib.load(model_path)
+    return model 
+
+
+data,song_to_index = load_dataframe(cleaned_data_path)
+transformed_data = load_transformed_data(transformed_data_path)
+model = load_model(model_path)
+
 
 # title
 st.title("Welcome to Spotify Song Recommender ! ")
@@ -29,27 +57,33 @@ st.write("You Entered : ", song_name)
 k = st.selectbox("How many recommendations do you want ? ",[5,10,15,20],index=1)
 
 # button 
-if st.button("Get Recommendation"):
-    st.write("Recommendation for ",f"***{song_name}***")
-    recommendation = recommend(song_name,data,transformed_data,k)
+try :
+    if st.button("Get Recommendation"):
+        st.write("Recommendation for ",f"***{song_name}***")
+        recommendation = recommend(song_name,data,model,song_to_index,transformed_data,k)
 
-    # display recommendation
-    for ind,recommendation in recommendation.iterrows():
-        song_name = recommendation['name'].title()
-        artist_name = recommendation['artist'].title()
+        # display recommendation
+        for ind,recommendation in recommendation.iterrows():
+            song_name = recommendation['name'].title()
+            artist_name = recommendation['artist'].title()
 
-        if ind == 0:
-            st.markdown("## Currently Playing")
-            st.markdown(f"### **{song_name}** by **{artist_name}**")
-            st.audio(recommendation['spotify_preview_url'])
-            st.write('----')
+            if ind == 0:
+                st.markdown("## Currently Playing")
+                st.markdown(f"### **{song_name}** by **{artist_name}**")
+                st.audio(recommendation['spotify_preview_url'])
+                st.write('----')
 
-        elif ind == 1:
-            st.markdown("## Next Up")
-            st.markdown(f"### **{ind}.** **{song_name}** by **{artist_name}**")
-            st.audio(recommendation['spotify_preview_url'])
-            st.write('----')
-        else :
-            st.markdown(f"### **{ind}.** **{song_name}** by **{artist_name}**")
-            st.audio(recommendation['spotify_preview_url'])
-            st.write('----')
+            elif ind == 1:
+                st.markdown("## Next Up")
+                st.markdown(f"### **{ind}.** **{song_name}** by **{artist_name}**")
+                st.audio(recommendation['spotify_preview_url'])
+                st.write('----')
+            else :
+                st.markdown(f"### **{ind}.** **{song_name}** by **{artist_name}**")
+                st.audio(recommendation['spotify_preview_url'])
+                st.write('----')
+
+except Exception as e :
+    print(e)
+    st.markdown("### Oops ! Can not find song in my Data")
+    st.markdown("### Please try another song.")
--- src/content_based_filtering.py
+++ src/content_based_filtering.py
@@ -7,6 +7,7 @@ from category_encoders.count import CountEncoder
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.compose import ColumnTransformer
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.neighbors import NearestNeighbors
 from scipy.sparse import save_npz
 
 from src.data_cleaning import data_for_content_filtering
@@ -88,77 +89,91 @@ def save_transformed_data (tansformed_data,save_path):
     Returns:
     None
     """
-    
+ 
     # save the transformed data
     save_npz(save_path,tansformed_data)
 
 
 
-def calculate_similarity_scores(input_vector, transformed_data):
+def build_model(transformed_data):
     """
-    Calculate similarity scores between an input vector and a dataset using cosine similarity.
-    Args:
-        input_vector (array-like): The input vector for which similarity scores are to be calculated.
-        data (array-like): The dataset against which the similarity scores are to be calculated.
-    Returns:
-        array-like: An array of similarity scores.
+    Builds a nearest neightbor model using sparse matrix
+    parameter: transformed_data (sparse matrix)
+
+    returns : Trained NN Model 
     """
-
-    # calculate similarity scores
-    similarity_score = cosine_similarity(input_vector,transformed_data)
-
-    return similarity_score
+    model = NearestNeighbors(metric='cosine',algorithm='brute')
+    model.fit(transformed_data)
+    return model
 
 
+def save_model(model):
+    """
+    Saves the model into the "models" folder
+    parameter:
+    model :  A trained model
+    """
+    joblib.dump(model,"models/nearest_neighbor.joblib")
 
-def recommend(song_name ,songs_data ,transformed_data,k=10) :
+
+
+def recommend(song_name ,songs_data,model,song_to_index,transformed_data,k=10) :
     """
         Recommends top k songs similar to the given song based on content-based filtering.
 
         Parameters:
         song_name (str): The name of the song to base the recommendations on.
         songs_data (DataFrame): The DataFrame containing song information.
+        model : Trained Nearest Neighbour Model
+        songs_to_index : A precomputed dictionary of all the songs with their indexes
         transformed_data (sparse matrix): The transformed data matrix for similarity calculations.
         k (int, optional): The number of similar songs to recommend. Default is 10.
 
         Returns:
         DataFrame: A DataFrame containing the top k recommended songs with their names, artists, and Spotify preview URLs.
     """
+    try : 
+        # convert song name to lower case
+        song_name = song_name.lower()
 
-    # convert song name to lower case
-    song_name = song_name.lower()
+        # get the index of the songs
+        song_idx = song_to_index.get(song_name)
 
-    # filter out the song from the data
-    song_row = songs_data.loc[songs_data["name"]==song_name]
+        if song_idx is None:
+            raise AttributeError("Not found in our data")
+        else:
+            # generate input vector
+            input_vector = transformed_data[song_idx].reshape(1,-1)
 
-    if song_row.empty:
-        print("Song not found in our dataset. Please try another song.")
-    else:
-        # get the index of the song
-        song_index = song_row.index[0]
-        # generate input vector
-        input_vector = transformed_data[song_index].reshape(1,-1)
+            # find nearest neighbours -> returns indexes of K neighbours
+            distances, indices = model.kneighbors(input_vector,n_neighbors=k+1)
 
-        # calculate similarity scores
-        similarity_score = cosine_similarity(transformed_data,input_vector)
+            # top k songs' index
+            indices = indices.ravel()
 
-        # getting the top k songs' indexes
-        top_k_songs_index = np.argsort(similarity_score.ravel())[-k-1:][::-1]
+            # return the top k songs
+            top_k_list =(
+                        songs_data.iloc[indices]
+                        [["name","artist","spotify_preview_url"]]
+                        .reset_index(drop=True)
+                        )
 
-        # get the top k songs' names
-        top_k_songs = songs_data.iloc[top_k_songs_index]
-
-        # print the top k songs
-        top_k_list = top_k_songs[["name","artist","spotify_preview_url"]].reset_index(drop=True)
-
-        return top_k_list
-
+            return top_k_list
+    except Exception as e:
+        print("Error occurred : ",e)
+        
 
 
-def test_recommendation(data_path,song_name, k = 10):
-    # convert song name to lower case
-    song_name = song_name.lower()
 
+def main(data_path):
+    """
+    Main function executing every other functions 
+
+    Parameters:
+    data_path (str): The path to the CSV file containing the song data.
+
+    Returns: None
+    """
     # load the data
     data = pd.read_csv(data_path)
 
@@ -174,27 +189,12 @@ def test_recommendation(data_path,song_name, k = 10):
     # save transformed data
     save_transformed_data(transformed_data, "data/processed/transformed_data.npz")
 
-    # filter out the song from the data
-    song_row = data.loc[data["name"]==song_name]
+    # build model 
+    model = build_model(transformed_data)
 
-    # get the index of the song
-    song_index = song_row.index[0]
-
-    # generate input vector
-    input_vector = transformed_data[song_index].reshape(1,-1)
-
-    # calculate similarity scores
-    similarity_score = cosine_similarity(transformed_data,input_vector)
-
-    # getting the top k songs' indexes
-    top_k_songs_index = np.argsort(similarity_score.ravel())[-k-1:][::-1]
-
-    # get the top k songs' names
-    top_k_songs = data.iloc[top_k_songs_index]
-
-    print(top_k_songs)
-
-
+    # save the model
+    save_model(model)
+    
 
 if __name__=="__main__":
-    test_recommendation(CLEANED_DATA_PATH,"Hips Don't Lie")
+    main(CLEANED_DATA_PATH)
